22c78a6182dc989a779ff37a8a47d80f2afe2239
maxtiw <yossapath.0405@gmail.com>
2026-09-26 22:59:19 +0700

    chore: remove scratch files

--- scratch/fixTS2.js
+++
@@ -1,4 +0,0 @@
-﻿const fs = require('fs');
-let lines = fs.readFileSync('app/dashboard/castle/page.tsx', 'utf8').split('\n');
-lines.splice(1288, 2); // Remove lines 1289 and 1290 (index 1288 and 1289)
-fs.writeFileSync('app/dashboard/castle/page.tsx', lines.join('\n'), 'utf8');

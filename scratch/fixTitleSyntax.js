07c2dda7da990a551b4b1597f309e1b4f2dd6cf6
maxtiw <yossapath.0405@gmail.com>
2026-09-26 23:21:57 +0700

    fix(castle): fix syntax error in TopHeader

---
+++ scratch/fixTitleSyntax.js
@@ -0,0 +1,6 @@
+﻿const fs = require('fs');
+let code = fs.readFileSync('components/TopHeader.tsx', 'utf8');
+
+code = code.replace(/""\/dashboard\/teams"/g, '"/dashboard/teams"');
+
+fs.writeFileSync('components/TopHeader.tsx', code, 'utf8');
